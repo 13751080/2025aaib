2a30864841833311564dbe1e32b47221ae9b1426
13751080 <dumachun@gmail.com>
2025-02-19 11:55:19 +0800

    week01

---
+++ week01-1.cpp
@@ -0,0 +1,16 @@
+/// week01-1.cpp 瘋狂程設 兩數相加
+/// File-New empty 開新的檔案
+/// Ctrl-S 存檔成 week01-1.cpp 要記得加 .cpp
+/// 下周考試考這道題,不用寫藍色的「中文註解」
+#include <stdio.h> ///檔名不能寫錯
+///stdio 的意思
+///
+int main()
+{
+	int a,b;
+	scanf("%d",&a);
+	scanf("%d",&b);
+
+	printf("%d",a+b);
+
+}
