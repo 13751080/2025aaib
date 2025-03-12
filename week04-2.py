b8562b7e2f68cda42282ff14765684dff2337f98
13751080 <dumachun@gmail.com>
2025-03-12 12:03:47 +0800

    week04

---
+++ week04-2.py
@@ -0,0 +1,15 @@
+// week04-2.py 琌さぱ LeetCode 珼驹肈 Easy 肈 ()
+// LeetCode 2529. Maximum Count of Positive Integer and Negative Integer
+int maximumCount(int* nums, int numsSize) {
+
+    int pos = 0, neg = 0; // 癹伴玡, 非称, 常0
+
+    for(int i=0; i<numsSize; i++){
+        if( nums[i] > 0) pos++; // タ计
+        if( nums[i] < 0) neg++; // 璽计
+    } // 癹伴い丁, 穝эウ
+
+     //癹伴,рウㄓノ
+    if(pos>neg) return pos; // タ计ゑ耕, 碞タ计
+    else return neg; // ぃ礛,碞璽计
+}
